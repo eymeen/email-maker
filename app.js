915b29d5424f832810a0c575c061b65e0bc412b8
Eymen Eid <eymeneid@gmail.com>
2024-01-20 17:58:58 +0300

    fix git

---
+++ app.js
@@ -0,0 +1,17 @@
+import EmailMaker from "email-maker";
+
+const button = document.querySelector("#generate")
+const input = document.querySelector("#email")
+
+button.addEventListener("click", _=>{
+    let username = input.value;
+    let file = new EmailMaker(username);
+
+    try{
+        if(file.check()){
+            file.download("frontend")
+        }
+    }catch(e){
+        console.log(e)
+    }
+})
